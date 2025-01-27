04c53931f12aad465cce3a9d133c170ba458af2b
giulio iacomino <giulio.iacomino92@gmail.com>
2025-01-27 11:23:39 +0000

    added some comments to keep notation of custom accelerated RMS calculation taken from old personal code

--- Source/AudioMeterProcessor.cpp
+++ Source/AudioMeterProcessor.cpp
@@ -70,3 +70,36 @@ float AudioMeterProcessor::getMagnitude(const juce::dsp::AudioBlock<float>& bloc
 
     return maxMagnitude;
 }
+
+// custom vectorised RMS calculation using apple accelerate - perhaps something similar is possible using juce::FloatVectorOperations ?!
+//float RMS(std::vector<float>& buffer, int start, int end){
+//    assert(start!=end);
+//    float sum = 0.0f;
+//
+//    // Compute the effective end index, considering the buffer size
+//    int effectiveEnd = std::min(end, static_cast<int>(buffer.size()));
+//    size_t length = effectiveEnd - start;
+//    if (length <= 0) {
+//        return 0.0f; // Handle invalid length
+//    }
+//
+//#ifdef __APPLE__
+//    const float* dataPtr = buffer.data() + start;
+//    vDSP_svesq(dataPtr, 1, &sum, length);
+//#else
+//#pragma clang loop vectorize(enable)
+//    for(int i=start;i<effectiveEnd;i++){
+//        float sampleValue = buffer[i];
+//
+//        sum+= sampleValue*sampleValue;
+//    }
+//#endif
+//
+//    sum/=(end-start);
+//    sum+=__FLT_EPSILON__;
+//    sum = sqrt(sum);
+//    if(isnan(sum)){
+//        int nanValue = 1;
+//    }
+//    return sum;
+//}
